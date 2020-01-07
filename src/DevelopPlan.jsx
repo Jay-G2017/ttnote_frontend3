1ecf3ad75ee809d7e5a896cf6c0365789e40ebc5
jay.gao <jay.gao@alo7.com>
2020-01-07 09:56:44 +0800

    feat: add develop plan, feedback, and home

--- src/DevelopPlan.jsx
+++ src/DevelopPlan.jsx
@@ -10,12 +10,12 @@ function DevelopPlan() {
       <HomeBody>
         <HomePaddingContainer>
           <h2 style={{marginBottom: '2rem'}}>功能开发计划</h2>
-          <h4>蕃茄时光会持续进行迭代，下面是后续会改进的功能</h4>
-          <p>理论上所有的浏览器都是可以正常工作的， 但是由于作者精力有限，还无法对所有主流的浏览器做兼容性测试。目前只推荐chrome</p>
-          <h4>手机端</h4>
-          <p>对于Ios系统，由于苹果会屏蔽后台的声音播放，所以当你锁屏或者不在浏览器页面的时候，完成蕃茄后会听不到提示音. 这个问题后续可能会通过发送邮件的方式来解决，或者在手机端会开发一个小程序</p>
-          <p>对于Android系统，目前还没测试</p>
-
+          <h4 style={{marginBottom: '1rem'}}>蕃茄时光会持续进行迭代，下面是后续会改进的功能</h4>
+          <ol>
+            <li>开发蕃茄统计的功能，可以查看每日，每周的蕃茄详情</li>
+            <li>任务列表支持拖动排序</li>
+            <li>开发项目搜索功能，可以搜索项目内容</li>
+          </ol>
         </HomePaddingContainer>
       </HomeBody>
       <HomeFooter/>
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import HomeHeader from "./HomeHeader";
+import {HomeContainer, HomePaddingContainer, HomeBody} from "./common/style";
+import HomeFooter from "./HomeFooter";
 
 function Feedback() {
   return (
-    <div>Feedback</div>
+    <HomeContainer>
+      <HomeHeader/>
+      <HomeBody>
+        <HomePaddingContainer>
+          <h2 style={{marginBottom: '2rem'}}>用户反馈</h2>
+          <h4 style={{marginBottom: '1rem'}}>如果你有任何建议或者有bug需要反馈，欢迎发送给我</h4>
+          <a href='mailTo:jay.g@ttnote.cn'>jay.g@ttnote.cn</a>
+        </HomePaddingContainer>
+      </HomeBody>
+      <HomeFooter/>
+    </HomeContainer>
   )
 }
 
