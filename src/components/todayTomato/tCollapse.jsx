9084bd57d60c7f7e5a17bd8ea055e7d7dea2ce3e
jay.gao <jay.gao@alo7.com>
2020-07-18 23:17:47 +0800

    feat: TCollapse ok

--- src/components/todayTomato/tCollapse.jsx
+++ src/components/todayTomato/tCollapse.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { IoMdArrowDropright } from 'react-icons/io';
+
+const Body = styled.div``;
+
+const TitleRow = styled.div`
+  display: flex;
+  align-items: center;
+  user-select: none;
+`;
+
+const RightIcon = styled(IoMdArrowDropright)`
+  &.open {
+    transform: rotate(90deg);
+    transition: transform 200ms;
+    font-size: 1.2rem;
+    color: ${window.ttnoteThemeLight.textColorTitle};
+    opacity: 1;
+  }
+  &.close {
+    transition: transform 200ms;
+    font-size: 1.2rem;
+    color: ${window.ttnoteThemeLight.textColorTitle};
+    opacity: 1;
+  }
+`;
 
 function TCollapse(props) {
-  return <div>{props.children}</div>;
+  const [open, setOpen] = useState(true);
+  return (
+    <Body>
+      <TitleRow
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <RightIcon className={open ? 'open' : 'close'} />
+        <div style={{ flex: 'auto' }}>{props.title || ''}</div>
+      </TitleRow>
+      {open && <div style={{ paddingLeft: '1rem' }}>{props.children}</div>}
+    </Body>
+  );
 }
 
 export default TCollapse;
