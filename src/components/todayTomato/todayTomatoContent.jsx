ae1a080be67955f57ff5a952086f49389e99ffce
jay.gao <jay.gao@alo7.com>
2020-07-18 22:17:52 +0800

    save

--- src/components/todayTomato/todayTomatoContent.jsx
+++ src/components/todayTomato/todayTomatoContent.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import TCollapse from './tCollapse';
+import {
+  IoIosMedical,
+  IoIosNutrition,
+  IoIosBowtie,
+  IoIosHeart,
+} from 'react-icons/io';
 
+import styled, { keyframes } from 'styled-components';
+
+const spinEffect = keyframes`
+  0% {transform: rotate(0deg)}
+  100% {transform: rotate(360deg)}
+`;
+
+const Icon1 = styled(IoIosHeart)`
+  animation: ${spinEffect} 1.2s linear infinite;
+  font-size: 60px;
+  color: pink;
+`;
+
+const Icon2 = styled(IoIosBowtie)`
+  animation: ${spinEffect} 1.2s linear infinite;
+  font-size: 60px;
+  color: red;
+`;
+
+const Icon3 = styled(IoIosNutrition)`
+  animation: ${spinEffect} 1.2s linear infinite;
+  font-size: 60px;
+  color: green;
+`;
+
+const Icon4 = styled(IoIosMedical)`
+  animation: ${spinEffect} 1.2s linear infinite;
+  font-size: 60px;
+  color: #3e80d3;
+`;
 function TodayTomatoContent(props) {
-  return <div>Hi</div>;
+  const { data } = props;
+
+  return (
+    <div>
+      {data.map((project) => (
+        <TCollapse>
+          <>
+            <div>body</div>
+            <Icon1 />
+            <Icon2 />
+            <Icon3 />
+            <Icon4 />
+          </>
+        </TCollapse>
+      ))}
+    </div>
+  );
 }
 
 export default TodayTomatoContent;
