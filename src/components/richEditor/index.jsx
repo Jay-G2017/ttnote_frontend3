54631f3fbab8329059d8e58edf4c618b26438724
gaojie <gaojie@wxcloud.com>
2020-11-23 20:11:17 +0800

    Feat: 初步插入了link,
    hover时显示了数据

--- src/components/richEditor/index.jsx
+++ src/components/richEditor/index.jsx
@@ -1,16 +1,19 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import isHotkey from 'is-hotkey';
+import isUrl from 'is-url'
 import { Editable, withReact, useSlate, Slate } from 'slate-react';
-import { Editor, Transforms, createEditor, Node } from 'slate';
+import { Editor, Transforms, createEditor, Range, Node } from 'slate';
 import { withHistory } from 'slate-history';
 import {
   MdFormatBold,
   MdFormatItalic,
   MdFormatUnderlined,
   MdCode,
+  MdLink,
 } from 'react-icons/md';
 
 import styled from 'styled-components';
+import { Popover } from 'antd';
 
 // import { Button, Icon, Toolbar } from '../components';
 
@@ -67,7 +70,7 @@ const RichEditor = (props) => {
   const [value, setValue] = useState(defaultValue);
   const renderElement = useCallback((props) => <Element {...props} />, []);
   const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
-  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+  const editor = useMemo(() => withLinks(withHistory(withReact(createEditor()))), []);
 
   return (
     <Slate
@@ -86,6 +89,7 @@ const RichEditor = (props) => {
         <MarkButton format="italic" icon="format_italic" />
         <MarkButton format="underline" icon="format_underlined" />
         <MarkButton format="code" icon="code" />
+        <LinkButton />
         {/* <BlockButton format="heading-one" icon="looks_one" />
         <BlockButton format="heading-two" icon="looks_two" />
         <BlockButton format="block-quote" icon="format_quote" />
@@ -168,6 +172,16 @@ const Element = ({ attributes, children, element }) => {
       return <li {...attributes}>{children}</li>;
     case 'numbered-list':
       return <ol {...attributes}>{children}</ol>;
+    case 'link':
+      return (
+        <Popover overlayStyle={{zIndex: 1051}} visible content={() => linkContent('www.baodu.com') } title={null}>
+        <a {...attributes} target={'_blank'} href={element.url}
+          onClick={() => window.open(element.url, '_blank') }
+        >
+          {children}
+        </a>
+        </Popover>
+      )
     default:
       return <p {...attributes}>{children}</p>;
   }
@@ -234,6 +248,12 @@ const renderIcon = (icon) => {
           <MdCode />
         </IconDiv>
       );
+   case 'link':
+      return (
+        <IconDiv>
+          <MdLink />
+        </IconDiv>
+      );
     default:
       break;
   }
@@ -260,5 +280,97 @@ const initialValue = [
     children: [{ text: '' }],
   },
 ];
+
+const withLinks = editor => {
+  const { insertData, insertText, isInline } = editor
+
+  editor.isInline = element => {
+    return element.type === 'link' ? true : isInline(element)
+  }
+
+  editor.insertText = text => {
+    if (text && isUrl(text)) {
+      wrapLink(editor, text)
+    } else {
+      insertText(text)
+    }
+  }
+
+  editor.insertData = data => {
+    const text = data.getData('text/plain')
+
+    if (text && isUrl(text)) {
+      wrapLink(editor, text)
+    } else {
+      insertData(data)
+    }
+  }
+
+  return editor
+}
+
+const insertLink = (editor, url) => {
+  if (editor.selection) {
+    wrapLink(editor, url)
+  }
+}
+
+const isLinkActive = editor => {
+  const [link] = Editor.nodes(editor, { match: n => n.type === 'link' })
+  return !!link
+}
+
+const unwrapLink = editor => {
+  Transforms.unwrapNodes(editor, { match: n => n.type === 'link' })
+}
+
+const wrapLink = (editor, url) => {
+  if (isLinkActive(editor)) {
+    unwrapLink(editor)
+  }
+
+  const { selection } = editor
+  const isCollapsed = selection && Range.isCollapsed(selection)
+  const link = {
+    type: 'link',
+    url,
+    children: isCollapsed ? [{ text: url }] : [],
+  }
+
+  if (isCollapsed) {
+    Transforms.insertNodes(editor, link)
+  } else {
+    Transforms.wrapNodes(editor, link, { split: true })
+    Transforms.collapse(editor, { edge: 'end' })
+  }
+}
+
+const LinkButton = () => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isLinkActive(editor)}
+      onMouseDown={event => {
+        event.preventDefault()
+        const url = window.prompt('Enter the URL of the link:')
+        if (!url) return
+        insertLink(editor, url)
+      }}
+    >
+       <IconDiv>
+          <MdLink />
+        </IconDiv>
+    </Button>
+  )
+}
+
+const linkContent = (url) => {
+  return (
+   <div>
+    <h2>www.bi</h2>
+  </div>
+  )
+} 
+  
 
 export default RichEditor;
